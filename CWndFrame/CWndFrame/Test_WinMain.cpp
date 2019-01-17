5dae554caee98e2f58afbbd615a1bdf3d863f8ee
fengyihua <1448331580@qq.com>
2019-01-17 09:41:26 +0800

    Add
    Daily supplement

--- CWndFrame/CWndFrame/Test_WinMain.cpp
+++ CWndFrame/CWndFrame/Test_WinMain.cpp
@@ -4,7 +4,14 @@
 
 int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
 {
-
+	CWndFrame MyWnd;
+	MyWnd.CWGetCmdShow(nShowCmd);
+	MyWnd.CWGetHinstance(hInstance);
+	MyWnd.CWGetStaticPoint(&MyWnd);
+	if (MyWnd.CWCreateWindowExA())
+	{
+		MyWnd.CWMsgProc();
+	}
 
 	return 0;
 }
